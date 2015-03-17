02d116ff33b3f40b222429c7c36f9afeadc4468b
Ben <511745256@qq.com>
2015-03-17 13:20:42 +0800

    creat

---
+++ CellTower/CellTower/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CellTower
+//
+//  Created by 刘奥明 on 15-3-17.
+//  Copyright (c) 2015年 MingTingChang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
