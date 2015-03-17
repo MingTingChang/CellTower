838ed47ee3a6ba9b9371c3caf4068396fb6777cb
liutingluhe <Ben@liuaomingdeMac.local>
2015-03-17 16:34:25 +0800

    素材

--- CellTower/CellTower/GameViewController.m
+++ CellTower/CellTower/GameViewController.m
@@ -34,8 +34,18 @@
 {
     [super viewDidLoad];
 
+    
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
     // Configure the view.
     SKView * skView = (SKView *)self.view;
+    
+    if (skView.scene) return;
+    
     skView.showsFPS = YES;
     skView.showsNodeCount = YES;
     /* Sprite Kit applies additional optimizations to improve rendering performance */
@@ -43,30 +53,11 @@
     
     // Create and configure the scene.
     GameScene *scene = [GameScene unarchiveFromFile:@"GameScene"];
+    scene.size = skView.bounds.size;
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     // Present the scene.
     [skView presentScene:scene];
-}
-
-- (BOOL)shouldAutorotate
-{
-    return YES;
-}
-
-- (NSUInteger)supportedInterfaceOrientations
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
 }
 
 - (BOOL)prefersStatusBarHidden {
