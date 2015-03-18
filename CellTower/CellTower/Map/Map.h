8ee9ea5871b0bb14162d010b64de6575f881251c
liutingluhe <Ben@liuaomingdeMac.local>
2015-03-18 22:31:21 +0800

    111
    111

--- CellTower/CellTower/Map/Map.h
+++ CellTower/CellTower/Map/Map.h
@@ -8,30 +8,7 @@
 
 #import <SpriteKit/SpriteKit.h>
 
-typedef enum
-{
-    MapTypeOneEnterDoor,         //1个出怪点
-    MapTypeTwoEnterDoor         //2个出怪点
-} MapType;
-
 @interface Map : SKSpriteNode
 
-@property (nonatomic , strong) NSMutableArray *grid;
-
-/** 栅格尺寸 */
-@property (nonatomic , assign , readonly) CGSize gridSize;
-
-/** 单位栅格所占像素点范围 gridPixel * gridPixel */
-@property (nonatomic , assign) int gridPixel;
-
-/** 地图类型 */
-@property (nonatomic , assign) MapType mapType;
-
-
-- (instancetype)initWithTexture:(SKTexture *)texture gridPixel:(int)pixel type:(MapType)type;
-+ (instancetype)spriteWithTexture:(SKTexture *)texture gridPixel:(int)pixel type:(MapType)type;
-
-
-- (NSArray *)findOutPathFrom:(CGPoint)from to:(CGPoint)to;
 
 @end
